f9a440aaf7289606a0fce6eff80c7bc215549234
Andrey Pokrovsky <iampokrovsky@gmail.com>
2022-03-25 16:36:49 +0400

    Добавляет решение задачи invertObject

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,12 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
+  if (!obj) {
+    return;
+  }
 
+  return Object.entries(obj).reduce((accum, [key, value]) => {
+    accum[value] = key;
+    return accum;
+  }, {});
 }
